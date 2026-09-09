c60461e3ccb891baf0564879c8e5ba16b45f3bac
vutienluc-gitgub <1081991@gmail.com>
2026-09-09 09:03:29 +0700

    update-new

--- src/shared/components/layout/PageLayout.tsx
+++ src/shared/components/layout/PageLayout.tsx
@@ -13,7 +13,9 @@ interface PageLayoutProps {
  */
 export function PageLayout({ children, className }: PageLayoutProps) {
   return (
-    <div className={cn('flex flex-col gap-6 min-w-0', className)}>
+    <div
+      className={cn('flex flex-col gap-6 min-w-0 flex-1 min-h-0', className)}
+    >
       {children}
     </div>
   );
